1883464f66bea754b33d2f7cc790d617314728ca
Mohamed Salman <mohamed.salman@valeo.com>
2023-12-02 15:24:47 +0200

    Add bastion host files

--- Demo/variables.tf
+++ Demo/variables.tf
@@ -1,3 +1,12 @@
+variable "project" {
+  description = "Project Name"
+  default     = "EKS-Automation-TF"
+}
+
+variable "environment" {
+  description = "Environment"
+  default     = "dev"
+}
 variable "region" {
   description = "AWS region"
   default     = "eu-west-1"
@@ -11,4 +20,26 @@ variable "vpc_cidr_block" {
 variable "availability_zones" {
   description = "Availability zones"
   default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
+
+## EC2 Bastion Host Variables
+variable "ec2-bastion-public-key-path" {
+  description = "ec2-bastion-public-key-path"
+  default     = "../secrets/ec2-bastion-key-pair.pub"
+}
+
+variable "ec2-bastion-private-key-path" {
+  description = "ec2-bastion-private-key-path"
+  default     = "../secrets/ec2-bastion-key-pair.pem"
+}
+
+variable "ec2-bastion-ingress-ip-1" {
+  description = "ec2-bastion-ingress-ip-1"
+  default     = "0.0.0.0/0"
+}
+
+variable "bastion-bootstrap-script-path" {
+  description = "bastion-bootstrap-script-path"
+  default     = "../Scripts/bastion-bootstrap.sh"
 }
---
+++ Scripts/bastion-bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# update packages
+sudo yum update --all -y
+# install packages
+sudo yum install readline-devel openssl-devel -y
+
